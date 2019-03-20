7686b1947cceea990304cd27e844b9350bdf8af9
Srijan Choudhary <srijan4@gmail.com>
2019-03-20 11:30:26 +0530

    Add deps cache for hex packages

--- plugins/deps_cache.mk
+++ plugins/deps_cache.mk
@@ -7,3 +7,18 @@ override define dep_fetch_git
 	git clone -q -n --reference ~/.gitcaches/$(CACHE_NAME).reference $(call dep_repo,$(1)) $(DEPS_DIR)/$(call dep_name,$(1)); \
 	cd $(DEPS_DIR)/$(call dep_name,$(1)) && git checkout -q $(call dep_commit,$(1)) && git repack -a -q && rm .git/objects/info/alternates;
 endef
+
+override define dep_fetch_hex
+	mkdir -p $(ERLANG_MK_TMP)/hex $(DEPS_DIR)/$1; \
+	$(eval TAR_PATH := $(ERLANG_MK_TMP)/hex/$1.tar) \
+	$(eval DEP_TAR_FILENAME := $1-$(strip $(word 2,$(dep_$1))).tar) \
+	$(eval REMOTE_PATH := https://repo.hex.pm/tarballs/$(DEP_TAR_FILENAME)) \
+	$(eval CACHE_PATH := ~/.gitcaches/$(DEP_TAR_FILENAME)) \
+	$(if $(wildcard $(CACHE_PATH)), \
+		$(eval ETAG := $(shell md5sum $(CACHE_PATH))) \
+		curl -H "if-none-match: $(ETAG)" -Lf$(if $(filter-out 0,$(V)),,s)o $(CACHE_PATH) $(REMOTE_PATH); \
+		,curl -Lf$(if $(filter-out 0,$(V)),,s)o $(CACHE_PATH) $(REMOTE_PATH); \
+	) \
+	cp $(CACHE_PATH) $(TAR_PATH); \
+	tar -xOf $(TAR_PATH) contents.tar.gz | tar -C $(DEPS_DIR)/$1 -xzf -;
+endef
